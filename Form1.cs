470849b407fd7565ff40b6e4d843db4e6073e8a3
sandesh <78782734+sandeshvue@users.noreply.github.com>
2021-03-08 10:40:58 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace newone
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+
+            string connString = @"Data Source=RNL002;Initial Catalog=mydatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            try
+            {
+                //sql connection object
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+
+                    //retrieve the SQL Server instance version
+                    string query = @"select * from dbo.db2tbl1";
+
+                    //define the SqlCommand object
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+
+                    //Set the SqlDataAdapter object
+                    SqlDataAdapter dAdapter = new SqlDataAdapter(cmd);
+
+                    //define dataset
+                    DataSet ds = new DataSet();
+
+                    //fill dataset with query results
+                    dAdapter.Fill(ds);
+
+                    //set DataGridView control to read-only
+                    dataGridView1.ReadOnly = true;
+
+                    //set the DataGridView control's data source/data table
+                    dataGridView1.DataSource = ds.Tables[0];
+
+
+                    //close connection
+                    conn.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                //display error message
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+
+        }
+    }
+}
